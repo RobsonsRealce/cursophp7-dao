e3256c2f18a0d970c43d9af97092d874750bfcf7
Robsons <robsons.e@terra.com.br>
2020-07-13 10:15:09 -0300

    metodos de lsta

--- index.php
+++ index.php
@@ -2,10 +2,31 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+//comentado por que vamos usar somente a classe Usuario
+/*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
 
+//usando a classe Usuario: só carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
+
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("Jo");
+//echo json_encode($search);
+
+//carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Elaine", "12");
+
+echo $usuario;
  ?>
